63bdcdf67041bfccf7da73ef4a0cbf24fc2b2905
prodchr <138140646+prodchr@users.noreply.github.com>
2026-05-22 14:33:07 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Mundial Predictor',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="el">
+      <body>{children}</body>
+    </html>
+  );
+}
